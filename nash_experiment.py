72db3836edc4e85bf85be91162ceccc659a428a6
Sarios <danielhp95@gmail.com>
2019-11-06 23:35:35 +0000

    Big refactoring. Removed pre-computed winrate_matrices and nash averagings changes to experiment parameters

--- nash_experiment.py
+++ nash_experiment.py
@@ -1,7 +1,7 @@
 import os
 import time
 from typing import List, Tuple
-import logging 
+import logging
 import pickle
 
 import torch
@@ -16,35 +16,35 @@ from regym.rl_algorithms import build_PPO_Agent, AgentHook
 
 from regym.game_theory import compute_winrate_matrix_metagame, compute_nash_averaging
 
+
 def experiment(task: Task, training_agent, self_play_scheme: SelfPlayTrainingScheme,
-                       checkpoint_at_iterations: List[int], base_path: str, seed: int):
-
-    logging.basicConfig(level=logging.INFO)
+               checkpoint_at_iterations: List[int], base_path: str, seed: int):
     logger = logging.getLogger(f'Experiment: Task: {task.name}. SP: {self_play_scheme.name}. Agent: {training_agent.name}')
-    logger.setLevel(logging.INFO)
 
-    # Because we care for reproducibility!
     np.random.seed(seed)
     torch.manual_seed(seed)
 
-    population = training_phase(task, training_agent, self_play_scheme, checkpoint_at_iterations, base_path, seed)
-    # TODO: refactor into single function which returns winrate_submatrices,
-    #       and evolution_maxent_nash_and_nash_averaging
-    winrate_matrix = compute_winrate_matrix_metagame(population, env=task.env,
-                                                     episodes_per_matchup=10)
-    logger.info('Computing winrate matrix')
-    winrate_submatrices = [np.array([[0.5]])] + [winrate_matrix[:i,:i] for i in range(2, len(winrate_matrix) + 1)] # TODO explain why we are appending
-    logger.info('Computing nash averagings for all submatrices')
-    evolution_maxent_nash_and_nash_averaging = [(np.array([1.]), np.array([0.]))] + \
-                                               [compute_nash_averaging(m, perform_logodds_transformation=True)
-                                                for m in winrate_submatrices[1:]]
-    logger.info('FINISHED computation! Moving to saving')
+    population = training_phase(task, training_agent, self_play_scheme,
+                                checkpoint_at_iterations, base_path)
+    logger.info('FINISHED training! Moving to saving')
+    winrate_submatrices, evolution_maxent_nash_and_nash_averaging = compute_optimality_metrics(population, task, logger)
     save_results(winrate_submatrices,
                  evolution_maxent_nash_and_nash_averaging,
                  checkpoint_at_iterations,
                  save_path=f'{base_path}/results')
     logger.info('FINISHED saving')
     logger.info('DONE')
+
+
+def compute_optimality_metrics(population, task, logger):
+    logger.info('Computing winrate matrix')
+    winrate_matrix = compute_winrate_matrix_metagame(population, env=task.env,
+                                                     episodes_per_matchup=10)
+    winrate_submatrices = [winrate_matrix[:i, :i] for i in range(1, len(winrate_matrix) + 1)]
+    logger.info('Computing nash averagings for all submatrices')
+    evolution_maxent_nash_and_nash_averaging = [compute_nash_averaging(m, perform_logodds_transformation=True)
+                                                for m in winrate_submatrices]
+    return winrate_submatrices, evolution_maxent_nash_and_nash_averaging
 
 
 def save_results(winrate_submatrices: List[np.ndarray],
@@ -59,7 +59,7 @@ def save_results(winrate_submatrices: List[np.ndarray],
 
 def save_winrate_matrices(winrate_submatrices, checkpoint_at_iterations, save_path):
     checkpoints_winrate_submatrices = {checkpoint: m
-                                       for checkpoint, m in 
+                                       for checkpoint, m in
                                        zip(checkpoint_at_iterations, winrate_submatrices)}
     pickle.dump(checkpoints_winrate_submatrices,
                 open(f'{save_path}/winrate_matrices.pickle', 'wb'))
@@ -73,7 +73,7 @@ def save_evolution_maxent_nash_and_nash_averaging(evolution_maxent_nash_and_nash
 
 
 def training_phase(task: Task, training_agent, self_play_scheme: SelfPlayTrainingScheme,
-               checkpoint_at_iterations: List[int], base_path: str, seed: int):
+                   checkpoint_at_iterations: List[int], base_path: str):
     """
     :param task: Task on which agents will be trained
     :param training_agent: agent representation + training algorithm which will be trained in this process
@@ -83,7 +83,7 @@ def training_phase(task: Task, training_agent, self_play_scheme: SelfPlayTrainin
     :param process_name: String name identifier
     :param base_path: Base directory from where subdirectories will be accessed to reach menageries, save episodic rewards and save checkpoints of agents.
     """
-    
+
     logger = logging.getLogger(f'TRAINING: Task: {task.name}. SP: {self_play_scheme.name}. Agent: {training_agent.name}')
     logger.info('Started')
 
@@ -109,7 +109,7 @@ def training_phase(task: Task, training_agent, self_play_scheme: SelfPlayTrainin
         save_trained_policy(trained_agent,
                             save_path=f'{trained_policy_save_directory}/{target_iteration}_iterations.pt',
                             logger=logger)
-        
+
         agents_to_benchmark += [trained_agent.clone()]
         training_agent = trained_agent # Updating
 
@@ -127,7 +127,7 @@ def train_for_given_iterations(env, training_agent, self_play_scheme,
                                         target_episodes=next_training_iterations, iteration=completed_iterations,
                                         menagerie=menagerie, menagerie_path=menagerie_path)
     training_duration = time.time() - training_start
-    logger.info('Training between iterations [{}, {}]: {} seconds'.format(
+    logger.info('Training between iterations [{}, {}]: {:.2} seconds'.format(
                 completed_iterations, completed_iterations + next_training_iterations,
                 training_duration))
     return menagerie, training_agent, trajectories
@@ -135,7 +135,7 @@ def train_for_given_iterations(env, training_agent, self_play_scheme,
 
 def save_trained_policy(trained_agent, save_path: str, logger):
     logger.info(f'Saving agent \'{training_agent.name}\' in \'{save_path}\'')
-    hooked_agent = AgentHook(trained_agent.clone(training=False), save_path=save_path)
+    AgentHook(trained_agent.clone(training=False), save_path=save_path)
 
 
 def ppo_config_dict():
@@ -151,7 +151,7 @@ def ppo_config_dict():
     config['ppo_ratio_clip'] = 0.2
     config['learning_rate'] = 3.0e-4
     config['adam_eps'] = 1.0e-5
-    config['horizon'] = 1024
+    config['horizon'] = 128
     config['phi_arch'] = 'MLP'
     config['actor_arch'] = 'None'
     config['critic_arch'] = 'None'
@@ -160,11 +160,13 @@ def ppo_config_dict():
 
 if __name__ == '__main__':
     task = generate_task('RockPaperScissors-v0')
-    sp_scheme = FullHistorySelfPlay
-    checkpoint_at_iterations = [10, 20, 30]
-    base_path= 'experiment-test'
-    seed = 69
-    training_agent = build_PPO_Agent(task, ppo_config_dict(), agent_name='Test')
+    sp_scheme = NaiveSelfPlay
+    config = ppo_config_dict()
+    checkpoint_at_iterations = list(range(0, config['horizon'] * 30, config['horizon'] + 1))
+    base_path = 'experiment-test'
+    seed = 100
+    training_agent = build_PPO_Agent(task, config, agent_name='Test')
+    logging.basicConfig(level=logging.INFO)
     experiment(task=task, self_play_scheme=sp_scheme,
                training_agent=training_agent,
                checkpoint_at_iterations=checkpoint_at_iterations,
